3ee3bad94a2e4f4386b4224b457d66eb41e83816
Emilikan <emilqiwi@gmail.com>
2019-04-27 22:38:28 +0300

    add image in hero

--- app/src/main/java/com/example/springhack/Hero.java
+++ app/src/main/java/com/example/springhack/Hero.java
@@ -1,9 +1,16 @@
 package com.example.springhack;
+import android.net.Uri;
+import android.os.Build;
 import android.support.annotation.NonNull;
 
+import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
 import android.widget.TextView;
+
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
@@ -11,7 +18,11 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.squareup.picasso.Picasso;
 
+import java.util.Objects;
 import java.util.Random;
 
 public class Hero extends AppCompatActivity {
@@ -19,10 +30,12 @@ public class Hero extends AppCompatActivity {
     private TextView heroInfo;
     private String name;
     private String info;
+    private String imageId;
     private FirebaseDatabase database = FirebaseDatabase.getInstance();
     private DatabaseReference myRef;
     private FirebaseAuth mAuth;
     private FirebaseUser user;
+    private ImageView imageView;
 
 
 
@@ -32,6 +45,7 @@ public class Hero extends AppCompatActivity {
         setContentView(R.layout.activity_hero);
         heroName =  findViewById(R.id.heroName);
         heroInfo =  findViewById(R.id.heroInfo);
+        imageView = findViewById(R.id.imageView2);
 
         user = mAuth.getInstance().getCurrentUser();
 
@@ -39,14 +53,25 @@ public class Hero extends AppCompatActivity {
         final String numberOfHero = random.nextInt(2)+"";
         myRef = database.getReference();
         myRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @RequiresApi(api = Build.VERSION_CODES.KITKAT)
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
                 name = dataSnapshot.child("hero").child(numberOfHero).child("name").getValue(String.class);
                 info = dataSnapshot.child("hero").child(numberOfHero).child("info").getValue(String.class);
+                imageId = dataSnapshot.child("hero").child(numberOfHero).child("info").getValue(String.class);
                 heroName.setText(name);
                 heroInfo.setText(info);
                 myRef.child("users").child("hero").child("name").setValue(name);
                 myRef.child("users").child("hero").child("info").setValue(info);
+
+                FirebaseStorage storage = FirebaseStorage.getInstance();
+                StorageReference storageRef = storage.getReferenceFromUrl(imageId);
+                storageRef.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                    @Override
+                    public void onSuccess(Uri uri) {
+                        Picasso.with(Hero.this).load(uri).into(imageView);
+                    }
+                });
 
             }
 
