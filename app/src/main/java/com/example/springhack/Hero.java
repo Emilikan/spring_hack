2b29aa8d58a9703f483c2be2152f5cc90663fcbd
dmitriy0 <sevdmitriy0804@mail.ru>
2019-04-27 20:46:43 +0300

    рÑегистрация

--- app/src/main/java/com/example/springhack/Hero.java
+++ app/src/main/java/com/example/springhack/Hero.java
@@ -1,18 +1,75 @@
 package com.example.springhack;
 
+import android.content.Intent;
+import android.os.Build;
+import android.support.annotation.NonNull;
+import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.Random;
 
 public class Hero extends AppCompatActivity {
-    String heroName;
-    String heroInfo;
+    TextView heroName;
+    TextView heroInfo;
+    String name;
+    String info;
+    FirebaseDatabase database = FirebaseDatabase.getInstance();
+    DatabaseReference myRef;
+    private FirebaseAuth mAuth;
+    FirebaseUser user = mAuth.getInstance().getCurrentUser();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hero);
-        heroName = ((TextView) findViewById(R.id.heroName)).getText().toString();
-        heroInfo = ((TextView) findViewById(R.id.heroInfo)).getText().toString();
+        heroName =  findViewById(R.id.heroName);
+        heroInfo =  findViewById(R.id.heroInfo);
+
+        final Random random = new Random();
+        final String numberOfHero = random.nextInt(2)+"";
+        myRef = database.getReference();
+        myRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                name = dataSnapshot.child("hero").child(numberOfHero).child("name").getValue(String.class);
+                info = dataSnapshot.child("hero").child(numberOfHero).child("info").getValue(String.class);
+                heroName.setText(name);
+                heroInfo.setText(info);
+                myRef.child("users").child("hero").child("name").setValue(name);
+                myRef.child("users").child("hero").child("info").setValue(info);
+
+
+            }
+
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+
+        });
+
+
+
     }
+
+
+
+
 }
+
+
--- app/src/main/java/com/example/springhack/MainActivity.java
+++ app/src/main/java/com/example/springhack/MainActivity.java
@@ -1,17 +1,40 @@
 package com.example.springhack;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Build;
 import android.os.Bundle;
 
+import android.preference.PreferenceManager;
+import android.support.annotation.NonNull;
 import android.support.annotation.RequiresApi;
 
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 
 public class MainActivity extends AppCompatActivity {
+    String password;
+    String login;
+    private FirebaseAuth.AuthStateListener mAuthStateListener;
+    private FirebaseAuth mAuth;
+    FirebaseDatabase database = FirebaseDatabase.getInstance();
+    DatabaseReference myRef;
+    FirebaseUser user = mAuth.getInstance().getCurrentUser();
     @RequiresApi(api = Build.VERSION_CODES.KITKAT)
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -21,19 +44,97 @@ public class MainActivity extends AppCompatActivity {
         Button signUp = findViewById(R.id.signUp);
 
 
+        login = ((EditText) findViewById(R.id.Email)).getText().toString();
+        password = ((EditText) findViewById(R.id.Password)).getText().toString();
+
 
         signIn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, SignIn.class);
-                startActivity(intent);
+                myRef = database.getReference();
+                myRef.addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                        if (dataSnapshot.child("users").child(user.getUid()).child("hero").getValue(String.class).equals("false")) {
+                            Toast.makeText(getApplicationContext(),"Нужен персонаж",Toast.LENGTH_SHORT).show();
+                        } else {
+                            Toast.makeText(getApplicationContext(),"Успешно",Toast.LENGTH_SHORT).show();
+
+                        }
+
+                    }
+
+
+                    @Override
+                    public void onCancelled(DatabaseError databaseError) {
+
+                    }
+                });
             }
         });
         signUp.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, SignUp.class);
-                startActivity(intent);
+                singInUser();
+            }
+        });
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
+    private void singInUser() {
+        login = ((EditText) findViewById(R.id.Email)).getText().toString();
+        password = ((EditText) findViewById(R.id.Password)).getText().toString();
+        mAuth = FirebaseAuth.getInstance();
+
+        mAuthStateListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    // sing in
+
+                } else {
+                    //sing out
+                }
+            }
+        };
+
+        mAuth.signInWithEmailAndPassword(login, password).addOnCompleteListener(this, new OnCompleteListener<AuthResult>()  {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()) {
+
+                    myRef = database.getReference();
+                    myRef.addListenerForSingleValueEvent(new ValueEventListener() {
+                        @Override
+                        public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                            if (dataSnapshot.child("users").child(user.getUid()).child("hero").getValue(String.class).equals("false")) {
+                                myRef.child("users").child(user.getUid()).child("hero").setValue("true");
+                                Intent intent = new Intent(MainActivity.this, SignUp.class);
+                                intent.putExtra("PARAM", 1);
+                                startActivity(intent);
+                                finish();
+                            } else {
+                                Toast.makeText(getApplicationContext(), "У вас  уже есть персонаж", Toast.LENGTH_SHORT).show();
+                            }
+
+                        }
+
+
+                        @Override
+                        public void onCancelled(DatabaseError databaseError) {
+
+                        }
+                    });
+
+
+
+
+
+
+                } else {
+                    Toast.makeText(getApplicationContext(), "Авторизация провалена", Toast.LENGTH_LONG).show();
+                }
             }
         });
     }
