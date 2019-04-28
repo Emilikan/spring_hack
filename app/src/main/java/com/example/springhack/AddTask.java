cf561633824c52725e6bacd89445ed259c9324e8
DanilaSuvorov <dansuvtim@gmail.com>
2019-04-28 14:16:27 +0300

    logic1

--- app/src/main/java/com/example/springhack/AddTask.java
+++ app/src/main/java/com/example/springhack/AddTask.java
@@ -1,5 +1,6 @@
 package com.example.springhack;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -14,6 +15,16 @@ public class AddTask extends AppCompatActivity {
         setContentView(R.layout.activity_add_task);
         String taskName = ((EditText) findViewById(R.id.taskName)).getText().toString();
         String taskHp = ((EditText) findViewById(R.id.taskHp)).getText().toString();
+
+        Button add = findViewById(R.id.button);
+        add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(AddTask.this, Enemy.class);
+                startActivity(intent);
+
+            }
+        });
 
         Button ok = findViewById(R.id.button);
         ok.setOnClickListener(new View.OnClickListener() {
